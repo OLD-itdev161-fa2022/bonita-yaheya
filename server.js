0a8be553cd3be7b37d58d41991df0b683f9125ca
BYaheya <89624914+BYaheya@users.noreply.github.com>
2021-09-15 21:07:02 -0500

    Initialize Activity 3

--- server.js
+++ server.js
@@ -7,12 +7,27 @@ const app = express();
 //Connect connectDatabase
 connectDatabase();
 
+// Configure Middleware
+app.use(express.json({ extended: false}));
+
 //API endpoints
+/**
+ * @route GET /
+ * @desc Test endpoint
+ */
 app.get('/', (req, res) => 
 res.send('http get request sent to root api endpoint')
 
 );
+/**
+ * @route GET /
+ * @desc Test endpoint
+ */
+app.post('/api/users', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+} )
 
 //Connection listener
-app.listen(8080, () => console.log('Express server running on port 8080'));
+app.listen(3000, () => console.log('Express server running on port 3000'));
 
