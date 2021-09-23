e460fc65a14cdcc1bd7f4de333398495308b9ec5
BYaheya <89624914+BYaheya@users.noreply.github.com>
2021-09-23 18:07:16 -0500

    Set up client app

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import connectDatabase from './config/db';
 import { check, validationResult } from 'express-validator';
+import cors from 'cors';
 
 //Initialize express application
 const app = express();
@@ -9,41 +10,47 @@ const app = express();
 connectDatabase();
 
 // Configure Middleware
-app.use(express.json({ extended: false}));
+app.use(express.json({ extended: false }));
+app.use(
+    cors({
+        origin: 'http://localhost:3000'
+    })
+);
 
 //API endpoints
 /**
  * @route GET /
  * @desc Test endpoint
  */
-app.get('/', (req, res) => 
-res.send('http get request sent to root api endpoint')
+app.get('/', (req, res) =>
+    res.send('http get request sent to root api endpoint')
 
 );
 /**
  * @route GET /
  * @desc Test endpoint
  */
-app.post('/api/users', 
-[
-    check('name', 'Please enter your name').not().isEmpty(),
-    check('email', 'Please enter a valid email').isEmail(),
-    check('password','Please enter a password with 6 or more characters').isLength({min: 6})
+app.post('/api/users',
+    [
+        check('name', 'Please enter your name').not().isEmpty(),
+        check('email', 'Please enter a valid email').isEmail(),
+        check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
 
-],
+    ],
 
-(req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(422).json({ errors: errors.array() });
-    }else {
-        return res.send(req.body);
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+        } else {
+            return res.send(req.body);
+        }
+        //console.log(req.body);
+        //res.send(req.body);
     }
-    //console.log(req.body);
-    //res.send(req.body);
-} 
 );
 
 //Connection listener
-app.listen(8080, () => console.log('Express server running on port 8080'));
+const port = 5000;
+app.listen(port, () => console.log(`Express server running on port ${port}`));
 
