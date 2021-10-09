5c99f3df33b984a92b6a666625e0cbcd8d09f10a
BYaheya <89624914+BYaheya@users.noreply.github.com>
2021-10-08 21:02:21 -0500

    Added user registration

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import connectDatabase from './config/db';
 import { check, validationResult } from 'express-validator';
 import cors from 'cors';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import User from './models/User';
 
 //Initialize express application
@@ -30,28 +30,55 @@ app.get('/', (req, res) =>
 
 );
 /**
- * @route GET /
- * @desc Test endpoint
+ * @route POST api/users
+ * @desc Register user
  * 
  */
 app.post('/api/users',
     [
         check('name', 'Please enter your name').not().isEmpty(),
         check('email', 'Please enter a valid email').isEmail(),
-        check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 }),
-        check('age', 'Please enter a valid age').not().isEmpty(),
+        check('password', 
+              'Please enter a password with 6 or more characters')
+              .isLength({ min: 6 }),
+
 
     ],
-
-    (req, res) => {
+      // Callback method
+    async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(422).json({ errors: errors.array() });
         } else {
-            return res.send(req.body);
+            const { name, email, password } = req.body;
+            try {
+                // Check if user exists
+                let user = await User.findOne({ email: email});
+                if (user) {
+                    return res
+                      .status(400)
+                      .json({ errors: [{ msg: 'User already exists'}] });
+
+                }
+
+                // Create a new user
+                user = new User({ 
+                    name: name,
+                    email: email,
+                    password: password    
+                });
+
+                // Encrypt the password
+                const salt = await bcrypt.genSalt(10);
+                user.password = await bcrypt.hash(password, salt);
+
+                // Save to the db and return
+                await user.save();
+                res.send('User sucessfully registered');
+            }   catch (error) {
+                res.status(500).send('Server error');
+            }
         }
-        //console.log(req.body);
-        //res.send(req.body);
     }
 );
 
