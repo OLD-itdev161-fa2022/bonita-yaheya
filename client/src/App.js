02a2947bdcfc85a5f1ac5eafbe08c2337c2007a7
BYaheya <89624914+BYaheya@users.noreply.github.com>
2021-11-10 19:37:29 -0600

    Updated App.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,24 +7,13 @@ import Login from './components/Login/Login';
 
 class App extends React.Component {
   state = {
-    data: null,
+    posts: [],
     token: null,
     user: null
-  }
+  };
 
   componentDidMount() {
-    axios.get('http://localhost:5000')
-    .then((response)  => {
-      this.setState({
-        data: response.data
-      })    
-
-   })
-   .catch((error) => {
-    console.error(`Error fetching data: ${error}`); 
-  })
-
-  this.authenticateUser();
+    this.authenticateUser();
 }
   authenticateUser = () => {
     const token = localStorage.getItem('token');
@@ -38,11 +27,19 @@ class App extends React.Component {
       headers: {
         'x-auth-token': token
       }
-    }
+    };
     axios.get('http://localhost:5000/api/auth', config)
     .then((response) => {
       localStorage.setItem('user', response.data.name)
-      this.setState({ user: response.data.name })
+      this.setState(
+        { user: response.data.name,
+          token: token
+         },
+         () => {
+           this.loadData();
+         }
+         
+         );
     })
     .catch(error => {
       localStorage.removeItem('user');
@@ -53,6 +50,29 @@ class App extends React.Component {
       
   }
 }
+loadData = () => {
+  const { token } = this.state;
+
+  if (token) {
+    const config = {
+      headers: {
+        'x-auth-token': token
+      }
+    };
+    axios
+      .get('http://localhost:5000/api/posts', config)
+      .then(response => {
+        this.setState({
+          posts: response.data
+
+        });
+
+      })
+      .catch(error => {
+        console.error(`Error fetching data: ${error}`);
+      });
+  }
+};
 
 logOut = () => {
   localStorage.removeItem('token');
@@ -61,10 +81,10 @@ logOut = () => {
 }
   
   render() {
-    let { user, data } = this.state;
+    let { user, posts } = this.state;
     const authProps = {
       authenticateUser: this.authenticateUser
-    }
+    };
 
     return ( 
       <Router>
@@ -88,15 +108,23 @@ logOut = () => {
         </header>
         <main>
           <Route exact path='/'>
-            {user ?
+            {user ? (
             <React.Fragment>
               <div>Hello {user} !</div>
-              <div>{data}</div>
-              </React.Fragment> :
+              <div>
+                {posts.map(post => (
+                  <div key={post._id}>
+                    <h1>{post.title}</h1>
+                    <p>{post.body}</p>
+                </div>
+                ))}
+                </div>
+              </React.Fragment> 
+             ) : (
               <React.Fragment>
                 Please Register or Login
                 </React.Fragment>
-            }
+            )}
           </Route>
           <Switch>
             <Route 
